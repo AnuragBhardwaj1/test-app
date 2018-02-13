f6f8f49aa0e9353f6cd2f0ed8118fbd08f8cdf80
Anurag Bhardwaj <anurag.bhardwaj@instantsys.com>
2018-02-13 11:01:40 +0530

    first commit

---
+++ src/app/reseter/reseter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reseter',
+  templateUrl: './reseter.component.html',
+  styleUrls: ['./reseter.component.css']
+})
+export class ReseterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
